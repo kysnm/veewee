8632e00ee2769f7fb6ec19d2c924c2dd1c39383d
TokyoIncidents <tokyoincidents.g@gmail.com>
2013-06-02 00:35:05 +0900

    CentOS_x86_64-netboot: extend timeout and remove ruby, puppet, chef
    ssh_login_timeout: 600 is too short
    no need puppet and chef

--- templates/CentOS-5.5-x86_64-netboot/postinstall.sh
+++ templates/CentOS-5.5-x86_64-netboot/postinstall.sh
@@ -16,27 +16,27 @@ yum -y install gcc bzip2 make kernel-devel-`uname -r`
 #yum -y update
 #yum -y upgrade
 
-yum -y install gcc-c++ zlib-devel openssl-devel readline-devel sqlite3-devel
+yum -y install gcc-c++ zlib-devel openssl-devel readline-devel sqlite-devel
 yum -y erase wireless-tools gtk2 libX11 hicolor-icon-theme avahi freetype bitstream-vera-fonts
 yum -y clean all
 
-#Installing ruby
-cd /tmp
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz || fail "Could not download Ruby source"
-tar xzvf ruby-1.9.2-p180.tar.gz 
-cd ruby-1.9.2-p180
-./configure
-make && make install
-cd /tmp
-rm -rf /tmp/ruby-1.9.2-p180
-rm /tmp/ruby-1.9.2-p180.tar.gz
-ln -s /usr/local/bin/ruby /usr/bin/ruby # Create a sym link for the same path
-ln -s /usr/local/bin/gem /usr/bin/gem # Create a sym link for the same path
+# #Installing ruby
+# cd /tmp
+# wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.2-p180.tar.gz || fail "Could not download Ruby source"
+# tar xzvf ruby-1.9.2-p180.tar.gz 
+# cd ruby-1.9.2-p180
+# ./configure
+# make && make install
+# cd /tmp
+# rm -rf /tmp/ruby-1.9.2-p180
+# rm /tmp/ruby-1.9.2-p180.tar.gz
+# ln -s /usr/local/bin/ruby /usr/bin/ruby # Create a sym link for the same path
+# ln -s /usr/local/bin/gem /usr/bin/gem # Create a sym link for the same path
 
-#Installing chef & Puppet
-echo "Installing chef and puppet"
-/usr/local/bin/gem install chef --no-ri --no-rdoc || fail "Could not install chef"
-/usr/local/bin/gem install puppet --no-ri --no-rdoc || fail "Could not install puppet"
+# #Installing chef & Puppet
+# echo "Installing chef and puppet"
+# /usr/local/bin/gem install chef --no-ri --no-rdoc || fail "Could not install chef"
+# /usr/local/bin/gem install puppet --no-ri --no-rdoc || fail "Could not install puppet"
 
 #Installing vagrant keys
 mkdir /home/vagrant/.ssh
